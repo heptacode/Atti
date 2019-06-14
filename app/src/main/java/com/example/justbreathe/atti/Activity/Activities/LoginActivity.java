f52b2c3be9aaddd8cc9f607051d1ce783d8f67e9
Hyunwoo K <k_hyunwoo@icloud.com>
2019-06-14 19:35:14 +0900

    Update LoginActivity for test

--- app/src/main/java/com/example/justbreathe/atti/Activity/Activities/LoginActivity.java
+++ app/src/main/java/com/example/justbreathe/atti/Activity/Activities/LoginActivity.java
@@ -1,18 +1,28 @@
 package com.example.justbreathe.atti.Activity.Activities;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.justbreathe.atti.Activity.Activities.Register.RegisterActivity1;
+import com.example.justbreathe.atti.Activity.Activities.Register.RegisterActivity2;
 import com.example.justbreathe.atti.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class LoginActivity extends AppCompatActivity {
     Button login, register;
     EditText id, pw;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +47,22 @@ public class LoginActivity extends AppCompatActivity {
                 //아이디 비번 서버 전송
                 //서버에서 이름(String) 한국인 여부(boolean) 받기
 
+                DocumentReference docRef = db.collection("accounts").document(sid);
+                docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot document = task.getResult();
+                            if (document.exists()) { // 이메일에 해당하는 레코드가 존재할 때
+                                Log.d("DB", "Doc DATA : " + document.getData());
+                            } else {
+                                Toast.makeText(LoginActivity.this, "계정 정보가 올바르지 않습니다.", Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            Log.d("DB", "get failed with ", task.getException());
+                        }
+                    }
+                });
 
 //                Call<LoginData> call = NetworkHelper.getInstance().signin(sid,spw);
 //                call.enqueue(new Callback<LoginData>() {
