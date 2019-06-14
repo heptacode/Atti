3c4fa10437ea92ac0857699246187b06a896c59a
Hyunwoo K <k_hyunwoo@icloud.com>
2019-06-14 19:25:30 +0900

    Add register w/ firebase

--- app/src/main/java/com/example/justbreathe/atti/Activity/Activities/Register/RegisterActivity5.java
+++ app/src/main/java/com/example/justbreathe/atti/Activity/Activities/Register/RegisterActivity5.java
@@ -1,19 +1,27 @@
 package com.example.justbreathe.atti.Activity.Activities.Register;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.example.justbreathe.atti.Activity.Activities.LoginActivity;
 import com.example.justbreathe.atti.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity5 extends AppCompatActivity {
-
     private LinearLayout login;
     private String str_email, str_pw, str_name;
     private boolean korean;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,17 +33,38 @@ public class RegisterActivity5 extends AppCompatActivity {
         str_email = prev.getStringExtra("email");
         str_pw = prev.getStringExtra("pw");
         str_name = prev.getStringExtra("name");
-        korean = prev.getBooleanExtra("korean",false);
+        korean = prev.getBooleanExtra("korean", false);
 
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //지금은 로그인 화면으로 돌아감
-                //쉐어드프리퍼런스에 위의 값 저장하기
-                Intent intent = new Intent(RegisterActivity5.this, LoginActivity.class);
-                startActivity(intent);
-                finish();
+
+                Map<String, Object> datas = new HashMap<>();
+                datas.put("korean", korean);
+                datas.put("name", str_name);
+                datas.put("passwd", str_pw);
+
+                db.collection("accounts").document(str_email)
+                        .set(datas)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+
+                                //지금은 로그인 화면으로 돌아감
+                                //쉐어드프리퍼런스에 위의 값 저장하기
+                                Intent intent = new Intent(RegisterActivity5.this, LoginActivity.class);
+                                startActivity(intent);
+                                finish();
+
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(RegisterActivity5.this, "통신 오류가 발생했습니다. 다시 시도하십시오.", Toast.LENGTH_SHORT).show();
+                            }
+                        });
             }
         });
 
