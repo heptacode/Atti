3310b80722a857f285b3cfc0c20eb513928e783b
Hyunwoo Kim <admin@hyunwoo.kim>
2019-06-15 18:58:15 +0900

    Create chatting

---
+++ app/src/main/java/com/example/justbreathe/atti/Activity/Object/Chat.java
@@ -0,0 +1,13 @@
+package com.example.justbreathe.atti.Activity.Object;
+
+public class Chat {
+    private String timestamp;
+    private String sender;
+    private String message;
+
+    public String timestamp() { return timestamp; }
+
+    public String sender() { return sender; }
+
+    public String messagne() { return message; }
+}
