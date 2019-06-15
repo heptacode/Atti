b61efb775f162f151fb5e2600d6490d2df7a925c
NightNMare <dlarbdnjs1@naver.com>
2019-06-15 20:21:16 +0900

    ...

--- app/src/main/java/com/example/justbreathe/atti/Activity/Activities/MainActivity.java
+++ app/src/main/java/com/example/justbreathe/atti/Activity/Activities/MainActivity.java
@@ -6,21 +6,49 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.justbreathe.atti.Activity.Activities.Recommend.RecommendActiivity;
 import com.example.justbreathe.atti.R;
 
 public class MainActivity extends AppCompatActivity {
-    LinearLayout ll;
+    ImageView menu;
+    DrawerLayout drawerLayout;
+    LinearLayout chat,recommend,med_conv;
+    TextView profile_name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ll = findViewById(R.id.intent1);
-        ll.setOnClickListener(new View.OnClickListener() {
+        chat=findViewById(R.id.drawer_chat);
+        recommend=findViewById(R.id.drawer_recommend);
+        med_conv=findViewById(R.id.drawer_med_conv);
+        profile_name=findViewById(R.id.drawer_name);
+
+        nameAppearance();
+
+        recommend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.closeDrawers();
+                Intent intent = new Intent(MainActivity.this,RecommendActiivity.class);
+                startActivity(intent);
+            }
+        });
+        menu = findViewById(R.id.main_menu);
+        drawerLayout=findViewById(R.id.drawer_layout);
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(Gravity.LEFT);
+            }
+        });
+        med_conv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //일단 누르면 자동로그인 풀림
@@ -28,10 +56,16 @@ public class MainActivity extends AppCompatActivity {
                 SharedPreferences.Editor mEditor = mprefs.edit();
                 mEditor.clear();
                 mEditor.apply();
-
-                Intent intent = new Intent(MainActivity.this, RecommendActiivity.class);
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
                 startActivity(intent);
+                finish();
             }
         });
     }
+    private void nameAppearance(){
+        SharedPreferences mprefs = getSharedPreferences("Profile_Data",MODE_PRIVATE);
+        String name = mprefs.getString("S_name","Null");
+        profile_name.setText(name);
+    }
+
 }
